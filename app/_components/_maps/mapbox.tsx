2009c575ae050737da682a01982d70f9c90d434c
Jeison Fox <jeison.fox@evolvingweb.com>
2023-07-10 18:48:20 -0500

    Semantec Demo - Adding right type for mapContainer.

--- app/_components/_maps/mapbox.tsx
+++ app/_components/_maps/mapbox.tsx
@@ -9,7 +9,7 @@ export default function MapBoxMap({
 }: {
   mapboxToken: string;
 }): JSX.Element {
-  const mapContainer = useRef<string | HTMLElement>(null);
+  const mapContainer = useRef<HTMLDivElement>(null);
   const map = useRef<mapboxgl.Map | null>(null);
 
   mapboxgl.accessToken = mapboxToken;
